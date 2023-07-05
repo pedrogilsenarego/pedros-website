b7dbfc4114a1dc9b73f4af1cb7bd2b8becf22ae6
pedrogilsenarego <pedrogilsenarego@gmail.com>
2023-07-05 23:47:54 +0100

    a

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,12 @@ import { CssBaseline, StyledEngineProvider } from "@mui/material";
 import { BrowserRouter } from "react-router-dom";
 import AppRoutes from "./routes";
 
-
 function App() {
   return (
     <BrowserRouter>
       <StyledEngineProvider injectFirst>
-
         <CssBaseline />
         <AppRoutes />
-
       </StyledEngineProvider>
     </BrowserRouter>
   );
