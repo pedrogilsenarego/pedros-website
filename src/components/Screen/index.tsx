d13a71d9c53344fb84f3734c0224742170860ecd
pedrogilsenarego <pedrogilsenarego@gmail.com>
2023-07-06 09:41:06 +0100

    a

--- src/components/Screen/index.tsx
+++ src/components/Screen/index.tsx
@@ -6,36 +6,49 @@ const Screen = () => {
 
   return (
     <>
-      <div
-        className="screen"
-        onClick={() => setPress(!press)}
-        style={{
-          cursor: "pointer",
-          transform: press
-            ? "perspective(800px) rotateX(-15deg)"
-            : "perspective(800px) rotateX(25deg)",
-          transition: "all 2s ease-in-out",
-        }}
-      >
-        <div className="screen-image" />
-        <div className="screen-overlay"></div>
-        <div className="screen-content">
-          <div className="screen-user">
-            <span className="name" data-value="PEDRO SENA REGO">
-              PEDRO SENA REGO
-            </span>
-            <span className="subname" data-value="PEDRO SENA REGO">
-              Web engineer
-            </span>
-            <a
-              className="link"
-              href="https://github.com/pedrogilsenarego"
-              target="_blank"
-              rel="noreferrer"
-            >
-              GitHub
-            </a>
+      <div>
+        <div
+          className="screen"
+          onClick={() => setPress(!press)}
+          style={{
+            cursor: "pointer",
+            transform: press
+              ? "perspective(800px) rotateX(-15deg)"
+              : "perspective(800px) rotateX(25deg)",
+            transition: "all 2s ease-in-out",
+          }}
+        >
+          <div
+            className="screen-image"
+            style={{
+              backgroundImage:
+                "url(https://avatars.githubusercontent.com/u/87972406?v=4)",
+            }}
+          />
+          <div className="screen-overlay"></div>
+          <div className="screen-content">
+            <div className="screen-user">
+              <span className="name" data-value="PEDRO SENA REGO">
+                PEDRO SENA REGO
+              </span>
+              <span className="subname" data-value="PEDRO SENA REGO">
+                Web engineer
+              </span>
+              <a
+                className="link"
+                href="https://github.com/pedrogilsenarego"
+                target="_blank"
+                rel="noreferrer"
+              >
+                GitHub
+              </a>
+            </div>
           </div>
+        </div>
+      </div>
+      <div style={{ marginLeft: "50px" }}>
+        <div className="screen" onClick={() => setPress(!press)}>
+          <div className="screen-overlay"></div>
         </div>
       </div>
     </>
